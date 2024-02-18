1cf1c94e16c4996be58950981ec4960b1e52c786
Ricardo <rcordero231@gmail.com>
2024-02-17 19:57:29 -0800

    initial commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    FLASK_DEBUG = 1
+
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#     FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
